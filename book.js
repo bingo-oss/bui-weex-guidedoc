4c4ecc627591e4fc5fc21900d9a245cdc903b6cc
tonyyls <yulsh@bingosoft.net>
2018-08-24 17:47:46 +0800

    修改文档主题

--- book.js
+++ book.js
@@ -3,40 +3,21 @@ module.exports = {
     author: '广州市品高软件股份有限公司',
     language: 'zh-hans',
 
-    links:{
-        'sidebar': {
-
-        }        
-    },
-
     plugins: [
         '-sharing',
-        //'-search',
         "-fontsettings",
-        //'include-codeblock',    //https://plugins.gitbook.com/plugin/include-codeblock
-        //"theme-comscore",        
-        //'prism',                //https://plugins.gitbook.com/plugin/prism
-        //'-highlight',
-        //'anchor-navigation-ex', //https://plugins.gitbook.com/plugin/anchor-navigation-ex
-        //'splitter',             //https://plugins.gitbook.com/plugin/splitter
-        'mermaid-gb3',            //https://plugins.gitbook.com/plugin/mermaid-gb3;http://knsv.github.io/mermaid/#demos
-        //'chart',                //https://plugins.gitbook.com/plugin/chart
-        //'emphasize',            //https://plugins.gitbook.com/plugin/emphasize
-        'cuav-chapters',
-        "terminal",
         'back-to-top-button',
-        "copy-code-button"
+        "copy-code-button",
+        "expandable-chapters",
+        "heading-anchors",
+        "theme-fexa"
     ],
 
-    pluginsConfig: {
-        /*
-        'anchor-navigation-ex': {
-            'showLevel': false           
-        } 
-        */
-    },
-
-    styles: {
-        website: 'styles/website.css'
+    "pluginsConfig": {
+        "theme-fexa":{
+            "search-placeholder":"输入关键字搜索", //搜索框提示信息
+            "logo":"./logo.png", //logo地址
+            "favicon":"./images/favicon.ico" //ico地址
+        }
     }
 };
